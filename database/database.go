716a063b572543d2024a0b01146877a8045324c7
uniqueisnfjllpbb <unicorn.dev88.1.0to100@gmail.com>
2023-12-10 06:40:33 +0900

    "postgresDB作成"

--- database/database.go
+++ database/database.go
@@ -1,8 +1,45 @@
 package database
 
 import (
-    "github.com/jinzhu/gorm"
-    
+	
+	"log"
+
+	"github.com/jinzhu/gorm"
+    _ "github.com/lib/pq" // ←これを追記
 )
 
 var d *gorm.DB
+
+// type User struct {
+//     gorm.Model
+//     Name  string
+//     Email string
+// }
+
+
+func InitDB() *gorm.DB {
+    
+
+    db, err := gorm.Open("postgres", `postgres://admin:@localhost/blg_general?sslmode=disable`)
+    
+    // "admin:passwordpassword@/blg_general?sslmode=disable"
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+
+
+    return db
+}
+
+
+
+// GetDB returns database connection
+func GetDB() *gorm.DB {
+    return d
+}
+
+// Close closes database
+func Close() {
+    d.Close()
+}
--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-import "net/http"
+import 
+(
+    "github.com/gin-gonic/gin"
+    "net/http"
+    "github.com/uniqueisnfjllpbb/blogging/database"
+    
+)
 
 func main() {
-    engine:= gin.Default()
-    engine.GET("/", func(c *gin.Context) {
+    r:= gin.Default()
+    database.InitDB()
+
+    
+    r.Run(":8081")
+    defer database.Close()
+
+    r.GET("/hello", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
             "message": "hello world",
         })
     })
-    engine.Run(":3000")
 }
