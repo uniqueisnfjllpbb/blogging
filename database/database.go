9132fe06398868c6347d6ae7545e54ff7286509f
uniqueisnfjllpbb <unicorn.dev88.1.0to100@gmail.com>
2023-12-11 08:31:12 +0900

    "エラー対応"

--- database/database.go
+++ database/database.go
@@ -3,11 +3,12 @@ package database
 import (
 	"fmt"
 	"log"
-	"os"
+	// "os"
 
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq" // ←これを追記
+     "gorm.io/driver/postgres"
 )
 
 var d *gorm.DB
@@ -21,15 +22,15 @@ var d *gorm.DB
 
 func InitDB() *gorm.DB {
     errEnv := godotenv.Load(".env")
-    db_host := os.Getenv("DB_HOST")
-    db_database := os.Getenv("DB_DATABASE")
-    db_username := os.Getenv("DB_USERNAME")
-    db_password := os.Getenv("DB_PASSWORD")
+    // db_host := os.Getenv("DB_HOST")
+    // db_database := os.Getenv("DB_DATABASE")
+    // db_username := os.Getenv("DB_USERNAME")
+    // db_password := os.Getenv("DB_PASSWORD")
     if errEnv != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", errEnv)
 	} 
     db, err := gorm.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable",db_username, db_password, db_host, db_database))
-    
+     
     // "admin:passwordpassword@/blg_general?sslmode=disable"
     if err != nil {
         log.Fatalln(err)
