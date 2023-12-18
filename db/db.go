a451746e022abbb95b1d593b8d8c7706cca87d62
uniqueisnfjllpbb <unicorn.dev88.1.0to100@gmail.com>
2023-12-18 19:13:18 +0900

    "DB設定"

--- db/db.go
+++ db/db.go
@@ -9,7 +9,6 @@ import (
 	"os"
 )
 
-
 func NewDB() *gorm.DB {
 	if os.Getenv("GO_ENV") == "dev" {
 		err := godotenv.Load()
@@ -18,10 +17,17 @@ func NewDB() *gorm.DB {
 		}
 	}
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
-	
-	db,err := gorm.Open(postgres.Open(url), &gorm.Config{})
-	if err !=nil {
+
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
 		log.Fatalln("接続に成功")
 	}
 	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close; err != nil {
+		log.Fatalln(err)
+	}
 }
