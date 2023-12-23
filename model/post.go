542bd88f755c6e7dae890ce0f10fae13f69a92aa
uniqueisnfjllpbb <unicorn.dev88.1.0to100@gmail.com>
2023-12-23 11:47:31 +0900

    Docker

---
+++ model/post.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Post struct {
+	ID        uuid.UUID `gorm:"primaryKey;size:255;default:uuid_generate_v4()"`
+	Title     string    `json:"title" gorm:"not null"`
+	Body      string    `json:"post" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+	UserId    uint      `json:"user_id" gorm:"not null"`
+}
+
+type Reply struct {
+	ID        uuid.UUID `gorm:"primaryKey;size:255;default:uuid_generate_v4()"`
+	Comment   string    `json:"comment" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
